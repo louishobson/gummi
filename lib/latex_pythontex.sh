fc64e6a5a3386bd991c86c460d5409bb3f1936fe
ldh35 <ldh35@cam.ac.uk>
2023-02-13 15:24:53 +0000

    Fixes so that pythontex can be used alongside includegraphics

--- lib/latex_pythontex.sh
+++ lib/latex_pythontex.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Args: flags, outdir, workfile
 
-latex $1 "$3" && pythontex "$3" && pdflatex $1 "$2" "$3"
+pdflatex $1 -interaction=nonstopmode "$3" && pythontex "$3" && pdflatex $1 -interaction=nonstopmode "$2" "$3"
 
 exit $?
